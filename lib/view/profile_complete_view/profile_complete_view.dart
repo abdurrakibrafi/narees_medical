fe26afeaa3d34f1ececbeed8c1176d1831d1d3a0
rafi <abdurrakibrafi469@gmail.com>
2025-04-12 15:12:37 +0600

    done

--- lib/view/profile_complete_view/profile_complete_view.dart
+++ lib/view/profile_complete_view/profile_complete_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, avoid_print
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, avoid_print, sort_child_properties_last
 
 import 'dart:io';
 
@@ -70,144 +70,171 @@ class _ProfileFillUpViewState extends State<ProfileFillUpView> {
       appBar: CustomAppBar(title: "Profile Complete"),
       backgroundColor: Colors.white,
       body: Container(
-        padding: AppPadding.bodyPadding,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            Center(
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: _imageFile != null
-                        ? Image.file(
-                            File(_imageFile!.path),
-                            width: 100,
-                            height: 100,
-                            fit: BoxFit.cover,
-                          )
-                        : Image.network(
-                            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-                            width: 100,
-                            height: 100,
-                            fit: BoxFit.cover,
-                          ),
-                  ),
-                  Positioned(
-                    bottom: 4,
-                    right: 0,
-                    child: GestureDetector(
-                      onTap: () => _showImagePickerOptions(context),
-                      child: Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.3),
-                              blurRadius: 4,
-                              offset: Offset(0, 2),
+          padding: AppPadding.bodyPadding,
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Center(
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: _imageFile != null
+                            ? Image.file(
+                                File(_imageFile!.path),
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              )
+                            : Image.network(
+                                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                      Positioned(
+                        bottom: 4,
+                        right: 0,
+                        child: GestureDetector(
+                          onTap: () => _showImagePickerOptions(context),
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(50),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.3),
+                                  blurRadius: 4,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                        child: Icon(
-                          Icons.camera_alt_outlined,
-                          color: Colors.blue,
-                          size: 22,
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              color: Colors.blue,
+                              size: 22,
+                            ),
+                          ),
                         ),
                       ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40),
+                CustomText(
+                  text: "Personal Information",
+                  fontSize: 15.h,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        fillColor: Color(0xFFE4E4E4),
+                        borderColor: Colors.transparent,
+                        hintText: "First Name",
+                        showObscure: false,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 40),
-            CustomText(
-              text: "Personal Information",
-              fontSize: 15.h,
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: CustomTextField(
-                    fillColor: Color(0xFFE4E4E4),
-                    borderColor: Colors.transparent,
-                    hintText: "First Name",
-                    showObscure: false,
+                    SizedBox(width: 12),
+                    Expanded(
+                      child: CustomTextField(
+                        fillColor: Color(0xFFE4E4E4),
+                        borderColor: Colors.transparent,
+                        hintText: "Last Name",
+                        showObscure: false,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                CustomTextField(
+                  fillColor: Color(0xFFE4E4E4),
+                  borderColor: Colors.transparent,
+                  hintText: "Email Address",
+                  showObscure: false,
+                ),
+                SizedBox(height: 20),
+                CustomTextField(
+                  fillColor: Color(0xFFE4E4E4),
+                  borderColor: Colors.transparent,
+                  hintText: "Phone number",
+                  showObscure: false,
+                ),
+                SizedBox(height: 20),
+                CustomTextField(
+                  fillColor: Color(0xFFE4E4E4),
+                  borderColor: Colors.transparent,
+                  hintText: "Registration Id",
+                  showObscure: false,
+                ),
+                SizedBox(height: 20),
+                CustomTextField(
+                  fillColor: Color(0xFFE4E4E4),
+                  borderColor: Colors.transparent,
+                  hintText: "Joined Date",
+                  showObscure: false,
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: CustomDropdown(
+                    label: 'Specialization',
+                    hint: 'Select Specialization',
+                    value: 'IV Therapy Specialist',
+                    items: [
+                      'IV Therapy Specialist',
+                      'General Practitioner',
+                      'Nurse',
+                      'Surgeon'
+                    ],
+                    onChanged: (value) {
+                      print(value);
+                    },
                   ),
                 ),
-                SizedBox(width: 12),
-                Expanded(
-                  child: CustomTextField(
-                    fillColor: Color(0xFFE4E4E4),
-                    borderColor: Colors.transparent,
-                    hintText: "Last Name",
-                    showObscure: false,
-                  ),
+                SizedBox(height: 20),
+                CustomText(
+                  text: "Upload Documents",
+                  fontSize: 15.h,
                 ),
+                SizedBox(height: 10),
+                Container(
+                    color: Color(0xFFE4E4E4),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(3),
+                          child: CustomText(
+                            text: "Choose file",
+                            fontSize: 14.h,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(color: Colors.grey),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        CustomText(
+                          text: "Add more image",
+                          color: Colors.grey,
+                          fontSize: 14.h,
+                        )
+                      ],
+                    )),
+                SizedBox(height: 30),
+
               ],
             ),
-            SizedBox(height: 20),
-            CustomTextField(
-              fillColor: Color(0xFFE4E4E4),
-              borderColor: Colors.transparent,
-              hintText: "Email Address",
-              showObscure: false,
-            ),
-            SizedBox(height: 20),
-            CustomTextField(
-              fillColor: Color(0xFFE4E4E4),
-              borderColor: Colors.transparent,
-              hintText: "Phone number",
-              showObscure: false,
-            ),
-            SizedBox(height: 20),
-            CustomTextField(
-              fillColor: Color(0xFFE4E4E4),
-              borderColor: Colors.transparent,
-              hintText: "Registration Id",
-              showObscure: false,
-            ),
-            SizedBox(height: 20),
-            CustomTextField(
-              fillColor: Color(0xFFE4E4E4),
-              borderColor: Colors.transparent,
-              hintText: "Joined Date",
-              showObscure: false,
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-
-              width: double.infinity,
-              child: CustomDropdown(
-                label: 'Specialization',
-                hint: 'Select Specialization',
-                value: 'IV Therapy Specialist',
-                items: [
-                  'IV Therapy Specialist',
-                  'General Practitioner',
-                  'Nurse',
-                  'Surgeon'
-                ],
-                onChanged: (value) {
-                  print(value);
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-            CustomText(
-              text: "Upload Documents",
-              fontSize: 15.h,
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
