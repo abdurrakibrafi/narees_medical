9b7e3e2ec9abcb0f09e04eb9b355018a28a85384
rafi <abdurrakibrafi469@gmail.com>
2025-04-17 14:57:42 +0600

    done

--- lib/view/nurse_dashboard/nurse_home_view/nurse_home_view.dart
+++ lib/view/nurse_dashboard/nurse_home_view/nurse_home_view.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:restaurent_discount_app/common%20widget/custom%20text/custom_text_widget.dart';
+import 'package:restaurent_discount_app/common%20widget/custom_button_widget.dart';
+import 'package:restaurent_discount_app/uitilies/app_colors.dart';
 import 'package:restaurent_discount_app/view/nurse_dashboard/nurse_home_view/widget/app_bar_widget_of_nurse.dart';
 
 class HomeViewForNurse extends StatelessWidget {
@@ -15,65 +19,122 @@ class HomeViewForNurse extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Order Supplies Button
               Container(
-                margin: EdgeInsets.only(bottom: 16),
-                width: double.infinity,
-                height: 50,
                 decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(25),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.blue.withOpacity(0.2),
+                      Colors.white,
+                      Colors.white,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'ORDER SUPPLIES',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: () {},
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Circle + Icon
+                          Container(
+                            width: 28,
+                            height: 28,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.blue.withOpacity(0.2),
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              color: AppColors.mainColor,
+                              size: 18,
+                            ),
+                          ),
+                          SizedBox(width: 12),
+                          CustomText(
+                            text: 'ORDER SUPPLIES',
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
+
+              SizedBox(height: 15),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomText(
+                    text: "Appointment Today",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.h,
+                  ),
+                  CustomText(
+                    text: "See All",
+                  )
+                ],
+              ),
+
+              SizedBox(height: 10),
 
               // Appointment Section
               Container(
                 padding: EdgeInsets.all(16),
                 decoration: BoxDecoration(
-                  color: Colors.blueAccent,
+                  color: Color(0xFF0071BC),
                   borderRadius: BorderRadius.circular(12),
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Appointment Today',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    CustomText(
+                      text: "John Date",
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(height: 12),
-                    Row(
-                      children: [
-                        Icon(Icons.person, color: Colors.white),
-                        SizedBox(width: 8),
-                        Text(
-                          'John Doe',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                      ],
+                    CustomText(
+                      text: 'Treatment Type: Hydration Therapy',
+                      color: Colors.white,
+                      fontSize: 16.h,
                     ),
                     SizedBox(height: 8),
-                    Text(
-                      'Treatment Type: Hydration Therapy',
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      children: [
-                        Icon(Icons.access_time, color: Colors.white),
-                        SizedBox(width: 8),
-                        Text(
-                          '10:00 AM, March 6, 2025 / 2/A. Florida, USA',
-                          style: TextStyle(color: Colors.white, fontSize: 12),
-                        ),
-                      ],
-                    ),
+                    Container(
+
+
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xFF519ed1),
+
+                      ),
+
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: [
+                              Icon(Icons.access_time,),
+                              SizedBox(width: 8),
+                              CustomText(
+                                text:
+                                    '10:00 AM, March 6, 2025 / 2/A. Florida, USA',
+                                color: Colors.black,
+                                fontSize: 12.h,
+                              ),
+                            ],
+                          ),
+                        ))
                   ],
                 ),
               ),
