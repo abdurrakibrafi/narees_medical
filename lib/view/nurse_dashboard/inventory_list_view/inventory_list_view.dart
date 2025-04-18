3ef005e69f570658ef75d8d62b37d075727c75a8
rafi <abdurrakibrafi469@gmail.com>
2025-04-18 14:53:40 +0600

    working

--- lib/view/nurse_dashboard/inventory_list_view/inventory_list_view.dart
+++ lib/view/nurse_dashboard/inventory_list_view/inventory_list_view.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:restaurent_discount_app/common%20widget/custom_app_bar_widget.dart';
+import 'package:restaurent_discount_app/common%20widget/custom_text_filed.dart';
 import 'package:restaurent_discount_app/view/nurse_dashboard/inventory_list_view/widget/inventory_card_widget.dart';
-
 
 class InventoryList extends StatelessWidget {
   final List<Map<String, String>> inventoryItems = [
@@ -32,37 +34,21 @@ class InventoryList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: CustomAppBar(title: "Inventory List"),
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Search bar
-            TextField(
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                hintText: 'Search here...',
-                hintStyle: TextStyle(color: Colors.black54),
-                prefixIcon: Icon(Icons.search, color: Colors.blue),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(color: Colors.blue),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(color: Colors.blue),
-                ),
-              ),
-            ),
+            CustomTextField(hintText: "Search", showObscure: false),
+
             SizedBox(height: 10),
-            // Showing result count
             Text(
               'Showing ${inventoryItems.length} Results',
               style: TextStyle(fontSize: 14, color: Colors.black54),
             ),
             SizedBox(height: 20),
-            // List of items
             Expanded(
               child: ListView.builder(
                 itemCount: inventoryItems.length,
@@ -78,5 +64,3 @@ class InventoryList extends StatelessWidget {
     );
   }
 }
-
-
