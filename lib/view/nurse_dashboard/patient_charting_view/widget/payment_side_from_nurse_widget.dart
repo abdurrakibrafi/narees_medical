27d1aad3775667f4c60c320d6795ecf2974f43b8
rafi <abdurrakibrafi469@gmail.com>
2025-04-25 17:18:17 +0600

    done

--- lib/view/nurse_dashboard/patient_charting_view/widget/payment_side_from_nurse_widget.dart
+++ lib/view/nurse_dashboard/patient_charting_view/widget/payment_side_from_nurse_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:restaurent_discount_app/uitilies/constant.dart';
