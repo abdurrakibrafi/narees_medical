e9a4acec9214db0a80bffe978b47fda270e71aab
rafi <abdurrakibrafi469@gmail.com>
2025-04-25 10:25:06 +0600

    one is done

--- lib/view/paitent_dashboard_view/paitient_profile_view/widget/patient_information.dart
+++ lib/view/paitent_dashboard_view/paitient_profile_view/widget/patient_information.dart
@@ -9,69 +9,73 @@ import '../../../nurse_dashboard/profile_view/widget/certification_card_widget.d
 class PatientInformationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(child: Container(
-        padding: EdgeInsets.all(15),
+    return SingleChildScrollView(
         child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border:
-                Border.all(color: Colors.grey.withOpacity(0.5), width: 2)),
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildDetailRow('First Name:', 'Anna'),
-                  Divider(),
-                  _buildDetailRow('Last Name:', 'Suraiya'),
-                  Divider(),
-                  _buildDetailRow('Email Address:', 'anna879@gmail.com'),
-                  Divider(),
-                  _buildDetailRow('Phone Number:', '(234) 555-1234'),
-                  Divider(),
-                  _buildDetailRow('Treatment Type:', 'Hydration IV Treatment'),
-                  Divider(),
-                  _buildDetailRow('Physical address:', '123/A, Florida, UK'),
-                  Divider(),
-                  _buildDetailRow('Location:', '123/A, Florida, UK'),
-                  Divider(),
-                  _buildDetailRow('Zip Code:', '1217'),
+            padding: EdgeInsets.all(15),
+            child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                        color: Colors.grey.withOpacity(0.5), width: 2)),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildDetailRow('First Name:', 'Anna'),
+                      Divider(),
+                      _buildDetailRow('Last Name:', 'Suraiya'),
+                      Divider(),
+                      _buildDetailRow('Email Address:', 'anna879@gmail.com'),
+                      Divider(),
+                      _buildDetailRow('Phone Number:', '(234) 555-1234'),
+                      Divider(),
+                      _buildDetailRow(
+                          'Treatment Type:', 'Hydration IV Treatment'),
+                      Divider(),
+                      _buildDetailRow(
+                          'Physical address:', '123/A, Florida, UK'),
+                      Divider(),
+                      _buildDetailRow('Location:', '123/A, Florida, UK'),
+                      Divider(),
+                      _buildDetailRow('Zip Code:', '1217'),
 
-                  SizedBox(height: 10.h),
+                      SizedBox(height: 10.h),
 
-                  // Document Section
-                  CustomText(
-                      text: 'Document',
-                      fontSize: 20.h,
-                      fontWeight: FontWeight.bold),
-                  SizedBox(height: 10.h),
-                  CertificateCard(
-                    certificateName: 'Hydration Therapy.pdf',
-                    fileSize: '153 Mb',
+                      // Document Section
+                      CustomText(
+                          text: 'Document',
+                          fontSize: 18.h,
+                          fontWeight: FontWeight.bold),
+                      CertificateCard(
+                        certificateName: 'Hydration Therapy.pdf',
+                        fileSize: '153 Mb',
+                      ),
+
+                      SizedBox(height: 10.h),
+
+                      CustomText(
+                          text: 'Patient ID',
+                          fontSize: 17.h,
+                          fontWeight: FontWeight.bold),
+                      SizedBox(height: 10.h),
+                      Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Image.network(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1VL1TA_ZSbMC9SQ-gtV_fxlDEImE8HWvbVxKea6F5aZ04YjpiecrhwwvCrX8jQQyUH0&usqp=CAU', // Replace with your image asset path
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: 150.h,
+                        ),
+                      ),
+                      SizedBox(height: 40.h),
+                    ],
                   ),
-
-                  SizedBox(height: 30.h),
-
-                  Text('Patient ID',
-                      style: TextStyle(
-                          fontSize: 20.h, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 10.h),
-                  Container(
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 1),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Image.asset(
-                      'assets/patient_id_card.png', // Replace with your image asset path
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                      height: 200.h,
-                    ),
-                  ),
-                ],
-              ),
-            ))));
+                ))));
   }
 
   Widget _buildDetailRow(String title, String value) {
